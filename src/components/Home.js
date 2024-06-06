b581bdfe55d81be628a6a1408e36d38767e7e0a5
Shivam maurya <krishnashivam735423@gmail.com>
2024-06-06 19:19:15 +0530

    ..

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,33 @@
 import React from "react";
 import pic from "./pic.jpg";
+import "../index.css";
 const Home = () => {
   return (
     <div id="home" className="section">
-      <div className="  bg-bg1 bg-cover bg-no-repeat sm:max-h-max xl:min-h-screen border-2 font-roboto  m-auto justify-items-center items-center  ">
-        <div className=" mt-36  w-11/12 border-2 grid xl:grid-cols-2 lg:grid-cols-2 sm:grid-rows-2   gap-2 p-2  m-auto">
-          <div className="  border-2 m-auto ">
-            <div className="mt-6 text-white xl:text-4xl lg:text-3xl md:text-3xl sm:text-2xl font-normal">
+      <div className="  bg-bg1 bg-cover bg-no-repeat sm:min-h-screen xl:max-h-screen border-2 font-roboto  m-auto justify-items-center items-center  ">
+        <div className=" mt-20  grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 sm:grid-rows-2   gap-1 p-2   m-auto">
+          <div className="   m-auto ">
+            <div className="mt-6 text-white xl:text-5xl lg:text-3xl md:text-3xl sm:text-2xl font-normal">
               Namasté,
             </div>
-            <div className="text-white xl:text-5xl lg:text-4xl md:text-3xl sm:text-3xl font-medium mt-3">
+            <div className="text-white xl:text-7xl   lg:text-6xl md:text-5xl sm:text-4xl font-medium md:mt-3 sm:mt-1">
               I'm <span className="text-yellow-300">Shivam</span>
             </div>
-            <div className="text-white xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl font-medium mt-3">
+            <div className="text-white xl:text-7xl lg:text-6xl md:text-5xl sm:text-4xl font-medium md:mt-3 sm:mt-1">
               Web developer
             </div>
-            <div className="text-white xl:text-xl lg:text-xl md:text-lg sm:text-sm  mt-3">
+            <div className="text-white xl:text-xl lg:text-xl md:text-lg sm:text-sm  md:mt-3 sm:mt-1">
               I am a skilled and passionate web developer with
             </div>
-            <div className="text-white xl:text-xl lg:text-xl md:text-lg sm:text-sm  ">
+            <div className="text-white xl:text-xl lg:text-xl md:text-lg sm:text-sm md:mt-3 sm:mt-1 ">
               experience in creating modern websites.
             </div>
           </div>
-          <div className="ml-20 border-2 m-auto">
+          <div className=" m-auto  mt-16 mb-16 ">
             <img
               src={pic}
               alt="pic"
-              className=" rounded-lg xl:h-60 xl:w-80 sm:h-40 sm:w-60 m-auto"
+              className="rounded-xl   md:xl-home-img sm:sm-home-img     "
             />
           </div>
         </div>
