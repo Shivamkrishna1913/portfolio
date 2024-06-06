6b9d4da5b1620e2417e360f8eb0a9d71a81c112a
Shivam maurya <krishnashivam735423@gmail.com>
2024-06-06 18:37:06 +0530

    ..

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,10 @@ const Home = () => {
       <div className="  bg-bg1 bg-cover bg-no-repeat sm:max-h-max xl:min-h-screen border-2 font-roboto  m-auto justify-items-center items-center  ">
         <div className=" mt-36  w-11/12 border-2 grid xl:grid-cols-2 sm:grid-rows-2   gap-2 p-2  m-auto">
           <div className="  border-2  ">
-            <div className="mt-6 text-white xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl font-normal">
+            <div className="mt-6 text-white xl:text-4xl lg:text-3xl md:text-3xl sm:text-2xl font-normal">
               Namasté,
             </div>
-            <div className="text-white xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl font-medium mt-3">
+            <div className="text-white xl:text-5xl lg:text-4xl md:text-3xl sm:text-3xl font-medium mt-3">
               I'm <span className="text-yellow-300">Shivam</span>
             </div>
             <div className="text-white xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl font-medium mt-3">
@@ -18,7 +18,7 @@ const Home = () => {
             <div className="text-white xl:text-xl lg:text-xl md:text-lg sm:text-sm  mt-3">
               I am a skilled and passionate web developer with
             </div>
-            <div className="text-white text-xl  ">
+            <div className="text-white xl:text-xl lg:text-xl md:text-lg sm:text-sm  ">
               experience in creating modern websites.
             </div>
           </div>
