4ee649ba84642160a55685aa4c161394c67c4721
Shivam maurya <krishnashivam735423@gmail.com>
2024-06-06 19:18:26 +0530

    ..

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <div id="home" className="section">
       <div className="  bg-bg1 bg-cover bg-no-repeat sm:max-h-max xl:min-h-screen border-2 font-roboto  m-auto justify-items-center items-center  ">
-        <div className=" mt-36  w-11/12 border-2 grid xl:grid-cols-2 sm:grid-rows-2   gap-2 p-2  m-auto">
-          <div className="  border-2  ">
+        <div className=" mt-36  w-11/12 border-2 grid xl:grid-cols-2 lg:grid-cols-2 sm:grid-rows-2   gap-2 p-2  m-auto">
+          <div className="  border-2 m-auto ">
             <div className="mt-6 text-white xl:text-4xl lg:text-3xl md:text-3xl sm:text-2xl font-normal">
               Namasté,
             </div>
@@ -22,8 +22,12 @@ const Home = () => {
               experience in creating modern websites.
             </div>
           </div>
-          <div className="ml-20 border-2 flex items-center justify-center">
-            <img src={pic} alt="pic" className=" rounded-lg" />
+          <div className="ml-20 border-2 m-auto">
+            <img
+              src={pic}
+              alt="pic"
+              className=" rounded-lg xl:h-60 xl:w-80 sm:h-40 sm:w-60 m-auto"
+            />
           </div>
         </div>
       </div>
