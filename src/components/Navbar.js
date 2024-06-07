04e3bc7652054acb83b7240ab85442031690c4a1
Shivam maurya <krishnashivam735423@gmail.com>
2024-06-07 07:39:16 +0530

    ..

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,12 @@ import logo from "./logo-color.png";
 const Navbar = () => {
   return (
     <div className="     p-5 fixed w-full      ">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex d:space-x-6  space-x-2 justify-between items-center">
         <div>
           <img src={logo} alt="logo" className="h-12 w-12 rounded-full" />
         </div>
 
-        <ul className="flex space-x-6">
+        <ul className="flex md:space-x-6  space-x-2">
           <li>
             <a className="text-gray-300 hover:text-white  " href="#home">
               Home
