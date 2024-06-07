720b4f3163afdd2b32bd357e7dde6b6555ee3f08
Shivam maurya <krishnashivam735423@gmail.com>
2024-06-07 11:09:06 +0530

    ...

--- src/components/Project.js
+++ src/components/Project.js
@@ -13,8 +13,8 @@ const Project = () => {
       id="projects"
       className="section w-screen bg-bg1 bg-cover bg-no-repeat font-roboto  "
     >
-      <div className=" md:h-screen max-h-max grid md:grid-cols-2 grid-rows-2 items-center pt-60 gap-10   md:pl-28 px-6 md:pb-20  pb-0 ">
-        <div className="border-2 border-white rounded-3xl h-96 w-96 bg-blue-900  ">
+      <div className=" md:h-screen max-h-max grid md:grid-cols-2 grid-rows-2 items-center pt-60 gap-10   md:pl-28   md:pb-20  pb-4 ">
+        <div className="border-2 border-white rounded-3xl h-96 w-96 bg-blue-900  mx-auto ">
           {/*logo name/skill link */}
           <div className=" w-full flex  p-8     ">
             <div className="m-auto w-1/5">
@@ -50,7 +50,7 @@ const Project = () => {
             <img src={facebook} alt="img" className="  rounded-xl" />
           </div>
         </div>
-        <div className="border-2 border-[#5F9EA0] rounded-3xl h-96 w-96 bg-[#097969] ">
+        <div className="border-2 border-[#5F9EA0] rounded-3xl h-96 w-96 bg-[#097969]  mx-auto ">
           {/*logo name/skill link */}
           <div className=" w-full flex  p-8     ">
             <div className="m-auto w-1/5    ">
@@ -86,80 +86,6 @@ const Project = () => {
           </div>
           <div className="p-4">
             <img src={chatterbox} alt="img" className="  rounded-xl" />
-          </div>
-        </div>
-      </div>
-      <div className=" md:h-screen max-h-max grid md:grid-cols-2 grid-rows-2 items-center pt-60 gap-2    md:pl-28 pl-8 md:pb-20  pb-20 ">
-        <div className="border-2 border-white rounded-3xl h-96 w-96 bg-blue-900  ">
-          {/*logo name/skill link */}
-          <div className=" w-full flex  p-8     ">
-            <div className="m-auto w-1/5">
-              <img src={flogo} alt="logo" className="  w-9 rounded-full" />
-            </div>
-            <div className="pl-2 m-auto w-3/5">
-              <div className="  text-white  font-medium   font-roboto text-xl">
-                Facebook-clone
-              </div>
-              <div className="text-sm text-slate-300 font-sans">HTML | CSS</div>
-            </div>
-            <div className="   m-auto w-1/5">
-              <button className="bg-sky-500 px-4 py-1 rounded-3xl   ">
-                <a href="https://shivam-facebook.vercel.app/" target="_blank">
-                  <div className="flex">
-                    <div className="font-medium">Visit</div>
-                    <div className="pt-1">
-                      <LuArrowUpRightFromCircle />
-                    </div>
-                  </div>
-                </a>
-              </button>
-            </div>
-          </div>
-
-          <div className="ml-5 mt-5 mb-5">
-            <p className="text-base text-slate-300">
-              {" "}
-              Facebook login page clone .
-            </p>
-          </div>
-          <div className="p-4">
-            <img src={facebook} alt="img" className="  rounded-xl" />
-          </div>
-        </div>
-        <div className="border-2 border-white rounded-3xl h-96 w-96 bg-blue-900  ">
-          {/*logo name/skill link */}
-          <div className=" w-full flex  p-8     ">
-            <div className="m-auto w-1/5">
-              <img src={flogo} alt="logo" className="  w-9 rounded-full" />
-            </div>
-            <div className="pl-2 m-auto w-3/5">
-              <div className="  text-white  font-medium   font-roboto text-xl">
-                Facebook-clone
-              </div>
-              <div className="text-sm text-slate-300 font-sans">HTML | CSS</div>
-            </div>
-            <div className="   m-auto w-1/5">
-              <button className="bg-sky-500 px-4 py-1 rounded-3xl   ">
-                <a href="https://shivam-facebook.vercel.app/" target="_blank">
-                  <div className="flex">
-                    <div className="font-medium">Visit</div>
-                    <div className="pt-1">
-                      <LuArrowUpRightFromCircle />
-                    </div>
-                  </div>
-                </a>
-              </button>
-            </div>
-          </div>
-
-          <div className="ml-5 mt-5 mb-5">
-            <p className="text-base text-slate-300">
-              {" "}
-              Facebook login page clone .
-            </p>
-          </div>
-          <div className="p-4">
-            <img src={facebook} alt="img" className="  rounded-xl" />
           </div>
         </div>
       </div>
